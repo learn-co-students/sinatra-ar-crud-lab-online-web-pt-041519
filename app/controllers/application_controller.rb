def5d4be5af6d83a3011e9a9e4c5e9b82e235d9c
aparkening <aaron.parkening@gmail.com>
2019-07-29 16:15:53 -0400

    Done. Sinatra talks to database with CRUD.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
-
 require_relative '../../config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -9,5 +7,67 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
   end
+
+  ### Create
+  # New form
+  get '/articles/new' do
+    erb :new
+  end
+
+  # Create instance
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    @article.save
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  
+  ### Read
+  # All
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  # Instance
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+
+  ### Update
+  # Edit form
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  # Update instance
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    @article.save
+
+    # Send new indicator to show
+    @new = "yes"
+
+    erb :edit
+  end
+
+
+  ### Delete
+  delete '/articles/:id/delete' do
+    article = Article.find(params[:id])
+    article.destroy
+
+    redirect '/articles'
+  end
+
 end
